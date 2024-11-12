bc2ea33fe2d58894fd63d62942806561f66d0ce6
NotedDevelopment <52609641+NotedDevelopment@users.noreply.github.com>
2024-11-11 20:10:01 -0800

    Update config.lua

--- noted-paradise/config.lua
+++ noted-paradise/config.lua
@@ -1,8 +1,8 @@
 Config = {}
 
 -- change later
-Config.Paradise = {coords = vector4(2339.44, 3051.93, 48.15, 273.39), r = { x = -35.0, y = 0.0, z = 32.92229 }, }
+Config.Paradise = {coords = vector3(3314, 6126, 6), r = { x = -28.700046539307, y = 0, z = -23.911205}, }
 Config.ParadiseCommand = "paradise"
 Config.Paradise.hideradar = true
 Config.TimeAllowedInParadise = 7000
-Config.CreateThreadWaitTimer = 5
+Config.CreateThreadWaitTimer = 0
